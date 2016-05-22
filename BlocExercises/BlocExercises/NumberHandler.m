151f5fae52686a3c2d9ae4f487fbaafa8d8c7de7
Catherine Washburn <cawashburn@gmail.com>
2016-05-21 21:55:50 -0400

    Assignment 15 completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,38 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    NSInteger num = [number integerValue];
+    NSInteger doubleInt = num * 2;
+    NSNumber *doubleNum = [NSNumber numberWithInt:doubleInt];
+    return doubleNum;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
+    NSMutableArray *arraySortNumbers = [[NSMutableArray alloc] init];
+    if (number<otherNumber){
+        for (int n = number; n <= otherNumber; n++)
+        {
+            [arraySortNumbers addObject:[NSNumber numberWithInt:n]];
+        }
+        return arraySortNumbers;
+    }
+    else{
+        for (int n = otherNumber; n <= number; n++)
+        {
+            [arraySortNumbers addObject:[NSNumber numberWithInt:n]];
+        }
+        return arraySortNumbers;
+    }
     return @[];
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSMutableArray *mutableArray = [arrayOfNumbers mutableCopy];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [mutableArray sortUsingDescriptors:@[sortDescriptor]];
+    return [mutableArray[0] longValue];
 }
 
 @end
